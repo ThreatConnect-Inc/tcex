36649fa4ef4d0124fcac88c3dcc872f82a7dd1d1
Bracey Summers <bsummers@threatconnect.com>
2018-05-10 14:25:58 -0500

    + update to safe_indicator method

--- tcex/tcex.py
+++ tcex/tcex.py
@@ -978,7 +978,10 @@ class TcEx(object):
             (string): The urlencoded string
         """
         if indicator is not None:
-            indicator = quote(self.s(str(indicator), errors=errors), safe='~')
+            try:
+                indicator = quote(self.s(str(indicator), errors=errors), safe='~')
+            except KeyError:
+                indicator = quote(bytes(indicator), safe='~')
         return indicator
 
     @staticmethod
