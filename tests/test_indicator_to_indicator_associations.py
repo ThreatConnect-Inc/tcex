b3a3947181bc222aa6b220f47974f150c04511e3
John Hurd <jhurd@threatconnect.com>
2018-10-11 17:06:23 -0400

    Adding more functionality and generalizing tcex_init functionality

--- tests/test_indicator_to_indicator_associations.py
+++ tests/test_indicator_to_indicator_associations.py
@@ -1,56 +1,49 @@
 # -*- coding: utf-8 -*-
 """Test the TcEx app.."""
 
-import traceback
+import os
+import sys
 
-from tcex import TcEx
-
-
-# TODO: implement the cleaning function to delete everything that was created
-# def clean_up():
-#     """Delete all of the indicators which were created."""
-#     pass
-
-# TODO: get these tests working
-# def verify_association_created(tcex):
-#     """."""
-#     resource = tcex.resource('Address')
-#     resource.resource_id('1.2.3.4')
-
-#     for result in resource:
-#         this_indicator_resource = tcex.resource('Address')
-#         this_indicator_resource.resource_id(result['data']['ip'])
-#         associated_asns = tcex.resource(tcex.safe_rt('ASN', lower=False))
-#         associations_resource = this_indicator_resource.associations(associated_asns)
-#         associations_results = associations_resource.request()
-#         assert len(associations_results['data']) == 1
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
+import utility
+import cleaner
 
 
-# def test_indicator_associations():
-#     """."""
-#     tcex = TcEx()
-#     args = tcex.args
-#     tcex.jobs.indicator({
-#         "summary": "1.2.3.4",
-#         "type": "Address",
-#     })
-#     tcex.jobs.indicator({
-#         "summary": "ASN1234",
-#         "type": tcex.safe_rt('ASN', lower=False),
-#     })
-#     tcex.jobs.association({
-#         'association_value': 'ASN1234',
-#         'association_type': tcex.safe_rt('ASN', lower=False),
-#         'resource_value': '1.2.3.4',
-#         'resource_type': 'Address'
-#     })
-#     tcex.jobs.process(args.api_default_org)
-#     assert len(tcex.jobs.indicator_results['failed']) == 0
-#     assert len(tcex.jobs.indicator_results['not_saved']) == 0
-#     assert len(tcex.jobs.indicator_results['saved']) == 2
+def verify_association_created(tcex):
+    """."""
+    resource = tcex.resource('Address')
+    resource.resource_id('4.5.6.7')
 
-#     verify_association_created(tcex)
+    for result in resource:
+        this_indicator_resource = tcex.resource('Address')
+        this_indicator_resource.resource_id(result['data']['ip'])
+        associated_asns = tcex.resource(tcex.safe_rt('ASN', lower=False))
+        associations_resource = this_indicator_resource.associations(associated_asns)
+        associations_results = associations_resource.request()
+        assert len(associations_results['data']) == 1
 
 
-# test_indicator_associations()
-# # clean_up()
+def test_indicator_associations():
+    """."""
+    tcex = utility.init_tcex()
+    tcex.jobs.indicator({
+        "summary": "4.5.6.7",
+        "type": "Address",
+    })
+    tcex.jobs.indicator({
+        "summary": "ASN1234",
+        "type": tcex.safe_rt('ASN', lower=False),
+    })
+    tcex.jobs.association({
+        'association_value': 'ASN1234',
+        'association_type': tcex.safe_rt('ASN', lower=False),
+        'resource_value': '4.5.6.7',
+        'resource_type': 'Address'
+    })
+    tcex.jobs.process(tcex.args.api_default_org)
+    assert len(tcex.jobs.indicator_results['failed']) == 0
+    assert len(tcex.jobs.indicator_results['not_saved']) == 0
+    assert len(tcex.jobs.indicator_results['saved']) == 2
+
+    verify_association_created(tcex)
+    cleaner.clean()
