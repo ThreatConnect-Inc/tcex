f1e7533a877e690961f6fa241bd7461142018e7d
Bracey Summers <bsummers@threatconnect.com>
2018-11-02 15:26:10 -0500

    + update version and docs.

--- tcex/__init__.py
+++ tcex/__init__.py
@@ -1,6 +1,6 @@
 """TcEx Framework"""
 __author__ = 'ThreatConnect (support@threatconnect.com)'
-__version__ = '0.8.22'
+__version__ = '0.8.23'
 __license__ = 'Apache License, Version 2'
 name = 'tcex'
 
