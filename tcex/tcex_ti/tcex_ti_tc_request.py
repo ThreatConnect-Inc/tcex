f0d619124263893933487e0290556e30aad459d6
Ben Purdy <bpurdy@threatconnect.com>
2019-03-31 11:04:27 -0400

    minor changes to add task tag support

--- tcex/tcex_ti/tcex_ti_tc_request.py
+++ tcex/tcex_ti/tcex_ti_tc_request.py
@@ -41,8 +41,6 @@ class TiTcRequest:
         else:
             url = '/v2/{}/{}'.format(main_type, sub_type)
 
-        print(self.tcex.session.post(url, json=data, params={'owner': owner}).text)
-
         return self.tcex.session.post(url, json=data, params={'owner': owner})
 
     def delete(self, main_type, sub_type, unique_id):
