13f14f95efeb043cc5388611784b82c060918ecf
Ben Purdy <bpurdy@threatconnect.com>
2019-04-09 15:15:49 -0400

    adding another test case

---
+++ tests/tcex_ti/test_victim_interface.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+"""Test the TcEx Threat Intel Module."""
+
+from ..tcex_init import tcex
+
+
+# pylint: disable=W0201
+class TestVictim:
+    """Test TcEx Victim."""
+
+    def setup_class(self):
+        """Configure setup before all tests."""
+        self.ti = tcex.ti
+
+    def test_victim_get(self, victim_id=None, name='victim-name-42353'):
+        """Test victim get."""
+        # create
+        if not victim_id:
+            victim_id = self.test_victim_create(name)
+
+        # get
+        ti = self.ti.victim(name, unique_id=victim_id)
+        r = ti.single(owner='TCI')
+        ti_data = r.json()
+        assert r.status_code == 200
+        assert ti_data.get('status') == 'Success'
+        assert ti_data.get('data').get(ti.api_entity).get('name') == name
+
+        # delete
+        self.test_victim_delete(victim_id)
+
+    def test_victim_get_attributes(self, victim_id=None, name='victim-name-12453'):
+        """Test victim get."""
+        # create
+        if not victim_id:
+            victim_id = self.test_victim_create(name)
+
+        self.test_victim_add_attribute(
+            victim_id=victim_id, attribute_type='Description', attribute_value='test1'
+        )
+        self.test_victim_add_attribute(
+            victim_id=victim_id, attribute_type='Description', attribute_value='test2'
+        )
+        self.test_victim_add_attribute(
+            victim_id=victim_id, attribute_type='Description', attribute_value='test3'
+        )
+
+        # get attributes
+        ti = self.ti.victim(name, unique_id=victim_id)
+        for attribute in ti.attributes():
+            assert attribute
+            break
+        else:
+            assert False
+
+        # delete
+        self.test_victim_delete(victim_id)
+
+    def test_victim_get_tags(self, victim_id=None, name='victim-name-64235'):
+        """Test victim get."""
+        # create
+        if not victim_id:
+            victim_id = self.test_victim_create(name)
+
+        self.test_victim_add_tag(victim_id=victim_id, tag='One')
+        self.test_victim_add_tag(victim_id=victim_id, tag='Two')
+
+        # get tags
+        ti = self.ti.victim(name, unique_id=victim_id)
+        for tag in ti.tags():
+            assert tag.get('name')
+            break
+        else:
+            assert False
+
+        # delete
+        self.test_victim_delete(victim_id)
+
+    def test_victim_get_include(self, victim_id=None, name='victim-name-78159'):
+        """Test victim get."""
+        if not victim_id:
+            victim_id = self.test_victim_create(name)
+
+        self.test_victim_add_attribute(
+            victim_id=victim_id, attribute_type='Description', attribute_value='test123'
+        )
+        self.test_victim_add_label(victim_id=victim_id, label='TLP:RED')
+        self.test_victim_add_tag(victim_id=victim_id, tag='PyTest')
+
+        parameters = {'includes': ['additional', 'attributes', 'labels', 'tags']}
+        ti = self.ti.victim(name, unique_id=victim_id)
+        r = ti.single(owner='TCI', params=parameters)
+        ti_data = r.json()
+        assert r.status_code == 200
+        assert ti_data.get('status') == 'Success'
+        assert ti_data.get('data').get('victim').get('name') == name
+        assert ti_data.get('data').get('victim').get('attribute')[0].get('value') == 'test123'
+        assert ti_data.get('data').get('victim').get('securityLabel')[0].get('name') == 'TLP:RED'
+        assert ti_data.get('data').get('victim').get('tag')[0].get('name') == 'PyTest'
+
+        # delete
+        self.test_victim_delete(victim_id)
+
+    def test_victim_create(self, name='victim-name-65341'):
+        """Test victim create."""
+        ti = self.ti.victim(name)
+        r = ti.create(owner='TCI')
+        ti_data = r.json()
+        assert r.status_code == 201
+        assert ti_data.get('status') == 'Success'
+        assert ti_data.get('data').get('victim').get('name') == name
+        return ti.unique_id
+
+    def test_victim_add_attribute(
+        self,
+        victim_id=None,
+        name='victim-name-nkjvb',
+        attribute_type='Description',
+        attribute_value='Example Description.',
+    ):
+        """Test victim attribute add."""
+
+        if not victim_id:
+            victim_id = self.test_victim_create(name)
+
+        ti = self.ti.victim(name, unique_id=victim_id)
+        r = ti.add_attribute(attribute_type=attribute_type, attribute_value=attribute_value)
+        attribute_data = r.json()
+        assert r.status_code == 201
+        assert attribute_data.get('status') == 'Success'
+        assert attribute_data.get('data').get('attribute').get('value') == attribute_value
+
+    def test_victim_add_label(self, victim_id=None, name='victim-name-ds4vb', label='TLP:GREEN'):
+        """Test victim attribute add."""
+        if not victim_id:
+            victim_id = self.test_victim_create(name)
+
+        ti = self.ti.victim(name, unique_id=victim_id)
+        r = ti.add_label(label=label)
+        label_data = r.json()
+        assert r.status_code == 201
+        assert label_data.get('status') == 'Success'
+
+    def test_victim_add_tag(self, victim_id=None, name='victim-name-fdsv23', tag='Crimeware'):
+        """Test victim attribute add."""
+        if not victim_id:
+            victim_id = self.test_victim_create(name)
+
+        ti = self.ti.victim(name, unique_id=victim_id)
+        r = ti.add_tag(tag)
+        tag_data = r.json()
+        assert r.status_code == 201
+        assert tag_data.get('status') == 'Success'
+
+    def test_victim_delete(self, victim_id=None, name='victim-name-bdsfd'):
+        """Test victim delete."""
+        # create indicator
+        if not victim_id:
+            victim_id = self.test_victim_create(name)
+
+        # delete indicator
+        ti = self.ti.victim(name, unique_id=victim_id)
+        r = ti.delete(owner='TCI')
+        ti_data = r.json()
+        assert r.status_code == 200
+        assert ti_data.get('status') == 'Success'
+
+    def test_victim_update(self, victim_id=None, name='victim-name-b3da3'):
+        """Test victim update."""
+        # create indicator
+        if not victim_id:
+            victim_id = self.test_victim_create(name)
+
+        name = 'victim-new-name-fdasb3'
+
+        # update indicator
+        ti = self.ti.victim(name, unique_id=victim_id)
+        r = ti.update(owner='TCI')
+        ti_data = r.json()
+        assert r.status_code == 200
+        assert ti_data.get('status') == 'Success'
+        assert ti_data.get('data').get('victim').get('name') == name
+
+        # delete indicator
+        self.test_victim_delete(victim_id)
