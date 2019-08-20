836656ae619753dcfa10b2b81cbc25adc2b1bb88
Bracey Summers <bsummers@threatconnect.com>
2019-08-20 09:42:53 -0500

    updated conftest.py for Apps to search for lib directory.

--- app_init/tests/conftest.py
+++ app_init/tests/conftest.py
@@ -5,6 +5,7 @@ import shutil
 import sys
 
 
+# clear log directory
 def clear_log_directory():
     """Clear the App log directory."""
     log_directory = 'log'
@@ -18,16 +19,53 @@ def clear_log_directory():
                 os.remove(file_path)
 
 
+def get_lib_directories():
+    """Get all "lib" directories."""
+    lib_directories = []
+    contents = os.listdir(os.getcwd())
+    for c in contents:
+        # ensure content starts with lib, is directory, and is readable
+        if c.startswith('lib') and os.path.isdir(c) and (os.access(c, os.R_OK)):
+            lib_directories.append(c)
+
+    # return sorted lib directories with newest Python version first
+    return sorted(lib_directories, reverse=True)
+
+
 def update_system_path():
     """Update the system path to ensure project modules and dependencies can be found."""
-    cwd = os.getcwd()
-    lib_dir = os.path.join(os.getcwd(), 'lib_')
-    lib_latest = os.path.join(os.getcwd(), 'lib_latest')
+    # set lib directory version based off current Python interpreter
+    lib_major_version = 'lib_{}'.format(sys.version_info.major)
+    lib_minor_version = '{}.{}'.format(lib_major_version, sys.version_info.minor)
+    lib_micro_version = '{}.{}'.format(lib_minor_version, sys.version_info.micro)
 
-    # insert the lib_latest directory into the system Path if no other lib directory found. This
-    # entry will be bumped to index 1 after adding the current working directory.
-    if not [p for p in sys.path if lib_dir in p]:
-        sys.path.insert(0, lib_latest)
+    cwd = os.getcwd()  # the current working directory
+
+    # get existing lib directories for current App
+    lib_directories = get_lib_directories()
+
+    # set default lib directory to lib_latest in case no other matches can be found (best effort)
+    lib_directory = os.path.join(cwd, 'lib_latest')
+
+    # search for most appropriate lib directory for the current version of Python
+    if lib_micro_version in lib_directories:
+        lib_directory = lib_micro_version  # exact micro version match
+    elif lib_minor_version in lib_directories:
+        lib_directory = lib_minor_version  # exact minor version match
+    elif lib_major_version in lib_directories:
+        lib_directory = lib_major_version  # exact major version match
+    else:
+        # no exact matches found, find closest match
+        for lv in [lib_micro_version, lib_minor_version, lib_major_version]:
+            for d in lib_directories:  # lib_directores are sorted newest to oldest version
+                if lv in d:
+                    lib_directory = d
+                    break
+            else:
+                continue
+            break
+
+    sys.path.insert(0, lib_directory)
 
     # insert the current working directory into the system Path for the App, ensuring that it is
     # always the first entry in the list.
