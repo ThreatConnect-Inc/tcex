6d5649c1147a6a78763e0fc13e0520a02d67578b
Bracey Summers <bsummers@threatconnect.com>
2020-01-10 21:28:27 -0600

    + updated artifacts

--- tcex/case_management/artifact.py
+++ tcex/case_management/artifact.py
@@ -3,7 +3,7 @@
 from .api_endpoints import ApiEndpoints
 from .common_case_management import CommonCaseManagement
 from .common_case_management_collection import CommonCaseManagementCollection
-from .note import Note, Notes
+from .filter import Filter
 from .tql import TQL
 
 
@@ -65,7 +65,7 @@ class Artifacts(CommonCaseManagementCollection):
     @property
     def filter(self):
         """Return instance of FilterArtifact Object."""
-        return FilterArtifact(self.tql)
+        return FilterArtifacts(ApiEndpoints.ARTIFACTS, self.tcex, self.tql)
 
 
 class Artifact(CommonCaseManagement):
@@ -73,24 +73,27 @@ class Artifact(CommonCaseManagement):
 
     Args:
         tcex (TcEx): An instantiated instance of TcEx object.
-        analytics_priority (str, kwargs): The Analytics Priority for the Artifact.
-        analytics_priority_level (int, kwargs): The Analytics Priority Level for the Artifact.
-        analytics_score (int, kwargs): The Analytics Score for the Artifact.
-        analytics_type (int, kwargs): The Analytics Type for the Artifact.
-        case_id (int, kwargs): The Case ID for the Artifact.
-        case_xid (str, kwargs): The unique Case XID for the Artifact.
-        date_added (date, kwargs): The Date Added for the Artifact.
-        file_data (base64, kwargs): The File Data for the Artifact.
-        intel_type (str, kwargs): The Intel Type for the Artifact.
-        links (dict, kwargs): The Intel Type for the Artifact.
-        notes (dict, kwargs): The Notes for the Artifact.
-        parent_case (dict, kwargs): The Parent Case for the Artifact.
-        parent_task (dict, kwargs): The Parent Task for the Artifact.
-        source (str, kwargs): The Source for the Artifact.
-        summary (str, kwargs): The Summary for the Artifact.
-        task_id (int, kwargs): The Task ID for the Artifact.
-        task_xid (str, kwargs): The Task xid for the Artifact.
-        type (str, kwargs): The Artifact Type for the Artifact.
+        analytics_priority (str, kwargs): [Read-Only] The **Analytics Priority** for the Artifact.
+        analytics_priority_level (int, kwargs): [Read-Only] The **Analytics Priority Level** for
+            the Artifact.
+        analytics_score (int, kwargs): [Read-Only] The **Analytics Score** for the Artifact.
+        analytics_type (str, kwargs): [Read-Only] The **Analytics Type** for the Artifact.
+        artifact_type (ArtifactType, kwargs): [Read-Only] The **Artifact Type** for the Artifact.
+        case_id (int, kwargs): [Required (alt: caseXid)] The **Case Id** for the Artifact.
+        case_xid (str, kwargs): [Required (alt: caseId)] The **Case Xid** for the Artifact.
+        date_added (str, kwargs): [Read-Only] The **Date Added** for the Artifact.
+        file_data (str, kwargs): Base64 encoded file attachment required only for certain artifact
+            types
+        intel_type (str, kwargs): The **Intel Type** for the Artifact.
+        links (Link, kwargs): The **Links** for the Artifact.
+        notes (Note, kwargs): a list of Notes corresponding to the Artifact
+        parent_case (Case, kwargs): [Read-Only] The **Parent Case** for the Artifact.
+        source (str, kwargs): The **Source** for the Artifact.
+        summary (str, kwargs): [Required] The **Summary** for the Artifact.
+        task (Task, kwargs): [Read-Only] The **Task** for the Artifact.
+        task_id (int, kwargs): the ID of the task which the Artifact references
+        task_xid (str, kwargs): the XID of the task which the Artifact references
+        type (str, kwargs): [Required] The **Type** for the Artifact.
     """
 
     def __init__(self, tcex, **kwargs):
@@ -108,19 +111,18 @@ class Artifact(CommonCaseManagement):
         self._file_data = kwargs.get('file_data', None)
         self._intel_type = kwargs.get('intel_type', None)
         self._links = kwargs.get('links', None)
-        self._notes = Notes(self.tcex, kwargs.get('notes', {}).get('data'))
-        # TODO: Fix me
+        self._notes = kwargs.get('notes', None)
         self._parent_case = kwargs.get('parent_case', None)
-        self._parent_task = kwargs.get('parent_task', None)
         self._source = kwargs.get('source', None)
         self._summary = kwargs.get('summary', None)
+        self._task = kwargs.get('task', None)
         self._task_id = kwargs.get('task_id', None)
         self._task_xid = kwargs.get('task_xid', None)
         self._type = kwargs.get('type', None)
 
     def add_note(self, **kwargs):
-        """Add a note to the artifact"""
-        self._notes.add_note(Note(self.tcex, **kwargs))
+        """Add a Note to a Case."""
+        self.notes.add_note(self.tcex.cm.note(**kwargs))
 
     @property
     def analytics_priority(self):
@@ -142,10 +144,11 @@ class Artifact(CommonCaseManagement):
         """Return the **Analytics Type** for the Artifact."""
         return self._analytics_type
 
-    # TODO: @bpurdy - do we want to create an object for each type ???
     @property
     def artifact_type(self):
         """Return the **Artifact Type** for the Artifact."""
+        if self._artifact_type:
+            return self.tcex.cm.artifact_type(**self._artifact_type)
         return self._artifact_type
 
     @property
@@ -154,50 +157,29 @@ class Artifact(CommonCaseManagement):
         return {'type': 'Artifact', 'value': self.summary, 'id': self.id}
 
     @property
-    def available_fields(self):
-        """Return the available fields to fetch for an Artifact."""
-        return [
-            'caseXid',
-            'caseId',
-            'fileData',
-            'intelType',
-            'notes',
-            'source',
-            'summary',
-            'taskId',
-            'taskXid',
-            'type',
-        ]
-
-    @property
     def case_id(self):
-        """Return the parent "Case ID" for the Artifact."""
+        """Return the parent **Case ID** for the Artifact."""
         return self._case_id
 
     @case_id.setter
     def case_id(self, case_id):
-        """Set the parent "Case ID" for the Artifact."""
+        """Set the parent **Case ID** for the Artifact."""
         self._case_id = case_id
 
     @property
     def case_xid(self):
-        """Return the "Case XID" for the Artifact."""
+        """Return the **Case XID** for the Artifact."""
         return self._case_xid
 
     @case_xid.setter
     def case_xid(self, case_xid):
-        """Set the "Case XID" for the Artifact."""
+        """Set the **Case XID** for the Artifact."""
         self._case_xid = case_xid
 
     @property
     def date_added(self):
-        """Return the "Date Added" for the Artifact."""
+        """Return the **Date Added** for the Artifact."""
         return self._date_added
-
-    @date_added.setter
-    def date_added(self, date_added):
-        """Set the "Date Added" for the Artifact."""
-        self._date_added = date_added
 
     def entity_mapper(self, entity):
         """Update current object with provided object properties.
@@ -210,204 +192,179 @@ class Artifact(CommonCaseManagement):
 
     @property
     def file_data(self):
-        """Return the "File Data" for the Artifact."""
+        """Return the **File Data** for the Artifact."""
         return self._file_data
 
     @file_data.setter
     def file_data(self, file_data):
-        """Set the "File Data" for the Artifact."""
+        """Set the **File Data** for the Artifact."""
         self._file_data = file_data
 
     @property
     def intel_type(self):
-        """Return the "Intel Type" for the Artifact."""
+        """Return the **Intel Type** for the Artifact."""
         return self._intel_type
 
     @intel_type.setter
     def intel_type(self, intel_type):
-        """Set the "Intel Type" for the Artifact."""
+        """Set the **Intel Type** for the Artifact."""
         self._intel_type = intel_type
 
     @property
     def links(self):
         """Return the **Links** for the Artifact."""
+        # TODO: create an object for this
         return self._links
 
     @property
     def notes(self):
-        """Return the "Notes" for the Artifact."""
+        """Return the **Notes** for the Artifact."""
+        if self._notes is None or isinstance(self._notes, dict):
+            notes = self._notes or {}
+            self._notes = self.tcex.cm.notes(initial_response=notes)
         return self._notes
 
     @notes.setter
     def notes(self, notes):
-        """Set the "Notes" for the Artifact."""
+        """Set the **Notes** for the Artifact."""
         self._notes = notes
 
     @property
     def parent_case(self):
-        """Return the **Parent Case** for the Artifact."""
+        """Return the **Parent Case** for the Task."""
+        if self._parent_case:
+            return self.tcex.cm.case(**self._parent_case)
         return self._parent_case
 
     @property
-    def parent_task(self):
-        """Return the **Parent Task** for the Artifact."""
-        return self._parent_task
-
-    @property
-    def required_properties(self):
-        """Return a list of required fields for an Artifact."""
-        return ['summary', 'type', 'intel_type', 'case_id']
-
-    @property
     def source(self):
-        """Return the "Source" for the Artifact."""
+        """Return the **Source** for the Artifact."""
         return self._source
 
     @source.setter
     def source(self, source):
-        """Set the "Source" for the Artifact."""
+        """Set the **Source** for the Artifact."""
         self._source = source
 
     @property
     def summary(self):
-        """Return the "Summary" for the Artifact."""
+        """Return the **Summary** for the Artifact."""
         return self._summary
 
     @summary.setter
     def summary(self, summary):
-        """Set the "Summary" for the Artifact."""
+        """Set the **Summary** for the Artifact."""
         self._summary = summary
 
     @property
+    def task(self):
+        """Return the **Task** for the Note."""
+        if self._task:
+            return self.tcex.cm.task(**self._task)
+        return self._task
+
+    @property
     def task_id(self):
-        """Return the "Task ID" for the Artifact."""
+        """Return the **Task ID** for the Artifact."""
         return self._task_id
 
     @task_id.setter
     def task_id(self, task_id):
-        """Set the "Task ID" for the Artifact."""
+        """Set the **Task ID** for the Artifact."""
         self._task_id = task_id
 
     @property
     def task_xid(self):
-        """Return the "Task XID" for the Artifact."""
+        """Return the **Task XID** for the Artifact."""
         return self._task_id
 
     @task_xid.setter
     def task_xid(self, task_xid):
-        """Set the "Task XID" for the Artifact."""
+        """Set the **Task XID** for the Artifact."""
         self._task_id = task_xid
 
     @property
     def type(self):
-        """Return the "Type" for the Artifact."""
+        """Return the **Type** for the Artifact."""
         return self._type
 
     @type.setter
     def type(self, artifact_type):
-        """Set the "Type" for the Artifact."""
+        """Set the **Type** for the Artifact."""
         self._type = artifact_type
 
 
-class FilterArtifact:
-    """Filter Object for Artifact
-
-    Args:
-        tql (TQL): Instance of TQL Class.
-    """
-
-    def __init__(self, tql):
-        """Initialize Class properties"""
-        self._tql = tql
+class FilterArtifacts(Filter):
+    """Filter Object for Artifacts"""
 
     def case_id(self, operator, case_id):
-        """Filter objects based on "case id" field.
+        """Filter Artifacts based on **caseid** keyword.
 
         Args:
-            operator (enum): The enum for the required operator.
-            case_id (int): The filter value.
+            operator (enum): The operator enum for the filter.
+            case_id (int): The ID of the case associated with this artifact.
         """
         self._tql.add_filter('caseid', operator, case_id, TQL.Type.INTEGER)
 
-    def hascase(self, operator, case):
-        """Filter objects based on "case" association.
+    def has_case(self, operator, has_case):
+        """Filter Artifacts based on **hascase** keyword.
 
         Args:
-            operator (enum): The enum for the required operator.
-            case (str): The filter value.
+            operator (enum): The operator enum for the filter.
+            has_case (int): A nested query for association to other cases.
         """
-        self._tql.add_filter('hascase', operator, case)
+        self._tql.add_filter('hascase', operator, has_case, TQL.Type.INTEGER)
 
-    def id(self, operator, artifact_id):
-        """Filter objects based on "ID" field.
+    def id(self, operator, id):  # pylint: disable=redefined-builtin
+        """Filter Artifacts based on **id** keyword.
 
         Args:
-            operator (enum): The enum for the required operator.
-            artifact_id (int): The filter value.
+            operator (enum): The operator enum for the filter.
+            id (int): The ID of the artifact.
         """
-        self._tql.add_filter('id', operator, artifact_id, TQL.Type.INTEGER)
-
-    @property
-    def keywords(self):
-        """Return supported TQL keywords."""
-        keywords = []
-        for prop in dir(self):
-            if prop.startswith('_') or prop in ['tql']:
-                continue
-            # remove underscore from method name to match keyword
-            keywords.append(prop.replace('_', ''))
-
-        return keywords
+        self._tql.add_filter('id', operator, id, TQL.Type.INTEGER)
 
     def note_id(self, operator, note_id):
-        """Filter objects based on "note id" field.
+        """Filter Artifacts based on **noteid** keyword.
 
         Args:
-            operator (enum): The enum for the required operator.
-            note_id (int): The filter value.
+            operator (enum): The operator enum for the filter.
+            note_id (int): The ID of the note associated with this artifact.
         """
         self._tql.add_filter('noteid', operator, note_id, TQL.Type.INTEGER)
 
     def source(self, operator, source):
-        """Filter objects based on "source" field.
+        """Filter Artifacts based on **source** keyword.
 
         Args:
-            operator (enum): The enum for the required operator.
-            source (str): The filter value.
+            operator (enum): The operator enum for the filter.
+            source (str): The source of the artifact.
         """
-        self._tql.add_filter('source', operator, source)
+        self._tql.add_filter('source', operator, source, TQL.Type.STRING)
 
     def summary(self, operator, summary):
-        """Filter objects based on "summary" field.
+        """Filter Artifacts based on **summary** keyword.
 
         Args:
-            operator (enum): The enum for the required operator.
-            summary (str): The filter value.
+            operator (enum): The operator enum for the filter.
+            summary (str): The summary of the artifact.
         """
-        self._tql.add_filter('summary', operator, summary)
+        self._tql.add_filter('summary', operator, summary, TQL.Type.STRING)
 
     def task_id(self, operator, task_id):
-        """Filter objects based on "task id" field.
+        """Filter Artifacts based on **taskid** keyword.
 
         Args:
-            operator (enum): The enum for the required operator.
-            task_id (str): The filter value.
+            operator (enum): The operator enum for the filter.
+            task_id (int): The ID of the task associated with this artifact.
         """
         self._tql.add_filter('taskid', operator, task_id, TQL.Type.INTEGER)
 
-    def tql(self, tql):
-        """Filter objects based on TQL expression.
-
-        Args:
-            tql (str): The raw TQL string for the filter.
-        """
-        self._tql.set_raw_tql(tql)
-
     def type_name(self, operator, type_name):
-        """Filter objects based on "type name" field.
+        """Filter Artifacts based on **typename** keyword.
 
         Args:
-            operator (enum): The enum for the required operator.
-            type_name (str): The filter value.
+            operator (enum): The operator enum for the filter.
+            type_name (str): The type name of the artifact.
         """
-        self._tql.add_filter('typename', operator, type_name)
+        self._tql.add_filter('typename', operator, type_name, TQL.Type.STRING)
