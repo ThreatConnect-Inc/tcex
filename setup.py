0909c3d4554cc9f4392e41e8b0d5ba8882e24075
Bracey Summers <bsummers@threatconnect.com>
2020-01-24 10:02:41 -0600

    + update to setup.py to exclude tests folder during install.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     install_requires=install_requires,
     license='Apache License, Version 2',
     name='tcex',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests', 'tests.*']),
     package_data={'': ['*.json']},
     scripts=scripts,
     url='https://github.com/ThreatConnect-Inc/tcex',
