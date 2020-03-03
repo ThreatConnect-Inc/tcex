bcbd67b3fc8cba799d15e6d4a611f610bfa42af1
Bracey Summers <bsummers@threatconnect.com>
2020-03-03 12:29:34 -0600

    + updated app templates

--- app_init/service_trigger/app.py
+++ app_init/service_trigger/app.py
@@ -12,6 +12,12 @@ class App(ServiceApp):
     def run(self):
         """Run the trigger logic."""
         while self.tcex.service.loop_forever(sleep=30):
+            # args defined in install.json/args.py with serviceConfig
+            # are available in self.args namespace
+            self.tcex.log.debug(f'Server configuration service_input {self.args.service_input}')
+
+            # any "extra" args passed to fire_event will be
+            # available in kwargs in the callback method
             self.tcex.service.fire_event(self.trigger_callback, my_data='data')
 
     def trigger_callback(self, playbook, trigger_id, config, **kwargs):
@@ -26,6 +32,14 @@ class App(ServiceApp):
         Returns:
             bool: True if playbook should trigger, False if not.
         """
+        # "extra" args passed to the fire_event method are available in kwargs
         my_data = kwargs.get('my_data')
         self.tcex.log.debug(f'my_data: {my_data}')
+
+        # args defined in install.json with serviceConfig set to False are available in config
+        self.tcex.log.debug(f"Playbook configuration playbook_input {config.get('playbook_input')}")
+
+        # write output variable
+        playbook.create_output('example.service_input', self.args.service_input)
+        playbook.create_output('example.playbook_input', config.get('playbook_input'))
         return True
