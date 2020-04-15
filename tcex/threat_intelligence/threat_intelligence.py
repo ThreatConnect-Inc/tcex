91f06ce40a358961bcdfe142c1523a995bb331f1
Ben Purdy <bpurdy@threatconnect.com>
2020-04-15 15:03:52 -0400

    fixing staging victims and geting victims by tags

--- tcex/threat_intelligence/threat_intelligence.py
+++ tcex/threat_intelligence/threat_intelligence.py
@@ -389,20 +389,23 @@ class ThreatIntelligence:
         associations = entity.pop('associations', [])
         security_labels = entity.pop('securityLabel', [])
         tags = entity.pop('tag', [])
-        entity_type = entity.pop('type')
+        entity_type = entity.pop('type', '').lower()
         file_content = None
-        if entity_type.lower() in ['document', 'report']:
+        if entity_type in ['document', 'report']:
             file_content = entity.pop('file_content', None) or entity.pop('fileContent', None)
         try:
             ti = self.indicator(entity_type, owner, **entity)
         except Exception:
-            entity['name'] = entity.pop('summary', None)
-            if entity_type.lower() in ['task']:
-                ti = self.task(owner=owner, **entity)
+            if entity_type in ['victim']:
+                ti = self.victim(owner=owner, **entity)
             else:
-                ti = self.group(entity_type, owner, **entity)
+                entity['name'] = entity.pop('summary', None)
+                if entity_type in ['task']:
+                    ti = self.task(owner=owner, **entity)
+                else:
+                    ti = self.group(entity_type, owner, **entity)
         r = ti.create()
-        if entity_type.lower() in ['document', 'report']:
+        if entity_type in ['document', 'report']:
             ti.file_content(file_content)
 
         data = {'status_code': r.status_code}
