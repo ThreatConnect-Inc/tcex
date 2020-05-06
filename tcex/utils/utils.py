84e50809caf1e73fa61955e925c60006767c3763
John Hurd <jhurd@threatconnect.com>
2020-05-06 13:39:22 -0400

    Removing duplicate functionality to shorten a group's name

--- tcex/utils/utils.py
+++ tcex/utils/utils.py
@@ -141,23 +141,6 @@ class Utils:
         return ''.join(random.choice(string.ascii_letters) for i in range(string_length))
 
     @staticmethod
-    def shorten_group_name(full_group_name: str) -> str:
-        """Shorten a group's name to be under the max of 100 characters.
-
-        Args:
-            full_group_name (str): The complete group name.
-
-        Returns:
-            str: A group name that is under the max limit.
-        """
-        MAX_GROUP_NAME_LENGTH = 100
-        if len(full_group_name) <= MAX_GROUP_NAME_LENGTH:
-            return full_group_name
-        else:
-            new_group_name = f'{full_group_name[:97]}...'
-            return new_group_name
-
-    @staticmethod
     def snake_to_camel(snake_string):
         """Convert snake_case to camelCase
 
