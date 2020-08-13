05df6858e2e9062470f20ed37a50daad2e5c6a9e
Ben Purdy <bpurdy@threatconnect.com>
2020-08-13 13:58:58 -0400

    updating model

--- tcex/stix/__init__.py
+++ tcex/stix/__init__.py
@@ -1,4 +1,3 @@
 """Playbooks module for TcEx Framework"""
 # flake8: noqa
 from .model import StixModel
-from .threat_actor import StixThreatActor
--- tcex/stix/model.py
+++ tcex/stix/model.py
@@ -1,7 +1,10 @@
 """Top-level Stix Model Class."""
 # standard library
-from typing import Union
-
+from .registry_key import WindowsRegistryKey
+from .url import URL
+from .ipv4 import IPv4Address
+from .ipv6 import IPv6Address
+from .cyber_observables import *
 # third-party
 import jmespath
 
@@ -11,36 +14,93 @@ class StixModel:
 
     def __init__(self):
         """Initialize Class properties."""
+        self._as_object = None
+        self._ipv4 = None
+        self._ipv6 = None
+        self._registry_key = None
+        self._url = None
+        self._domain_name = None
+        self._email_address = None
+
+    @property
+    def as_object(self):
+        if not self._as_object:
+            self._as_object = StixASObject()
+        return self._as_object
+
+    @property
+    def ipv4(self):
+        if not self._ipv4:
+            self._ipv4 = IPv4Address()
+        return self._ipv4
+
+    @property
+    def ipv6(self):
+        if not self._ipv6:
+            self._ipv6 = IPv6Address()
+        return self._ipv6
+
+    @property
+    def registry_key(self):
+        if not self._registry_key:
+            self._registry_key = WindowsRegistryKey()
+        return self._registry_key
+
+    @property
+    def url(self):
+        if not self._url:
+            self._url = URL()
+        return self._url
+
+    @property
+    def email_address(self):
+        if not self._email_address:
+            self._email_address = StixEmailAddressObject()
+        return self._email_address
+
+    @property
+    def domain_name(self):
+        if not self._domain_name:
+            self._domain_name = StixDomainNameObject()
+        return self._domain_name
 
     def produce(self, tc_data: Union[list, dict]):
-        type_mapping = {}
-        # type_mapping = {
-        #     'as object': AS(),
-        #     'domain name object': DomainName(),
-        #     'email address object': EmailAddress(),
-        #     'email message object': EmailMessage(),
-        #     'ipv4 address object': Ipv4(),
-        #     'ipv6 address object': Ipv6(),
-        #     'url object': Url(),
-        #     'windows registry key object': RegistryKey(),
-        #
-        # }
+
+        type_mapping = {
+            'asn': self.as_object,
+            'host': self.domain_name,
+            'emailaddress': self.email_address,
+            'address': self.ipv4,
+            'registry key': self.registry_key,
+            'url': self.url
+
+        }
         if not isinstance(tc_data, list):
             tc_data = [tc_data]
 
-        stix_data = []
         for data in tc_data:
-            type = data.get('type')
-            handler = type_mapping.get(type.lower())
-            stix_data.append(handler.produce(data))
-
-        flattened_stix_data = [y for x in stix_data for y in x]
-
-        for stix_data in flattened_stix_data:
-            yield stix_data
+            type = data.get('type').lower()
+            handler = type_mapping.get(type.lower(), type.lower())
+            yield from handler.produce(data)
 
     def consume(self, stix_data: Union[list, dict]):
-        raise NotImplementedError
+        type_mapping = {
+            'autonomous-system': self.as_object,
+            'domain-name': self.domain_name,
+            'email-addr': self.email_address,
+            'ipv4-addr': self.ipv4,
+            'ipv6-addr': self.ipv6,
+            'windows-registry-key': self.registry_key,
+            'url': self.url
+        }
+
+        if not isinstance(stix_data, list):
+            stix_data = [stix_data]
+
+        for data in stix_data:
+            type = data.get('type').lower()
+            handler = type_mapping.get(type.lower(), type.lower())
+            yield from handler.produce(data)
 
     @staticmethod
     def _map(data: Union[list, dict], mapping: dict):
@@ -54,11 +114,11 @@ class StixModel:
                 if isinstance(value, list):
                     new_list = []
                     for item in value:
-                        new_list.append(list(StixModel._map2(d, item))[0])
+                        new_list.append(list(StixModel._map(d, item))[0])
 
                     mapped_obj[key] = new_list
                 elif isinstance(value, dict):
-                    mapped_obj[key] = list(StixModel._map2(d, mapped_obj[key]))[0]
+                    mapped_obj[key] = list(StixModel._map(d, mapped_obj[key]))[0]
                 else:
                     if not value.startswith('@'):
                         mapped_obj[key] = value
