a86904d7b13390b5e4532bd1485f8cffbd2931f9
Chris Blades <cblades@threatconnect.com>
2020-08-17 15:47:04 -0400

    add first pass at indicator parsing

---
+++ tcex/stix/indicator/indicator.py
@@ -0,0 +1,165 @@
+from typing import Union, List, Dict
+
+from dendrol import Pattern
+from dendrol.lang.STIXPatternVisitor import STIXPatternVisitor
+from dendrol.lang.STIXPatternParser import STIXPatternParser
+import ipaddress
+
+from tcex.stix.StixParser import StixParser
+
+
+class STIXVisitor(STIXPatternVisitor):
+    def __init__(self):
+        super().__init__()
+        self._indicators = []  # indicators that have been pulled out of this
+
+    def visitPropTestEqual(self, ctx: STIXPatternParser.PropTestEqualContext):
+        test = ctx.getText()
+        path, value = test.split('=')
+
+        self._indicators.append({
+            'path': path.strip(),
+            'value': value[1:-2].strip()
+        })
+        return self.visitChildren(ctx)
+
+    # TODO implement in operator
+
+    @property
+    def indicators(self):
+        return self._indicators
+
+
+class Indicator(StixParser):
+    def produce(self, tc_data: Union[list, dict]):
+        if not isinstance(tc_data, list):
+            tc_data = [tc_data]
+
+        type_map = {
+            'Host': lambda data: f"[domain-name:value = '{data.get('summary')}']",
+            'URL': lambda data: f"[url:value = '{data.get('summary')}']",
+            'EmailAddress': lambda data: f"[email-addr:value = '{data.get('summary')}']",
+            'ASN': lambda data: f"[autonomous-system:name = '{data.get('summary')}']",
+            'Address': Indicator._address_producer_helper,
+            'CIDR': Indicator._cidr_producer_helper,
+            'File': Indicator._file_producer_helper,
+        }
+
+        for data in tc_data:
+            yield type_map.get(data.get('type'))()  # TODO handle unsupported types
+
+    def consume(self, stix_data: Union[list, dict]):
+        """Produce a ThreatConnect object from a STIX 2.0 JSON object."""
+        if not isinstance(stix_data, list):
+            stix_data = [stix_data]
+
+        for data in stix_data:
+            # TODO: handle other pattern_types
+            pattern = Pattern(data.get('pattern'))
+            s = STIXVisitor()
+            pattern.visit(s)
+            print(s.indicators)
+
+            yield from Indicator._default_consume_handler(s.indicators)
+            yield from Indicator._ip_consume_handler(s.indicators)
+            yield from Indicator._file_consume_handler(s.indicators)
+
+    @staticmethod
+    def _default_consume_handler(stix_indicators: List[Dict[str, str]]):
+        type_map = {
+            'url:value': 'URL',
+            'email-addr:value': 'EmailAddress',
+            'domain-name:value': 'Host',
+            'autonomous-system:name': 'ASN'
+        }
+
+        for i in filter(lambda i: i.get('path') in type_map.keys(), stix_indicators):
+            path = i.get('path')
+            value = i.get('value')
+            yield {
+                'type': type_map.get(path),
+                'value': value
+            }
+
+    @staticmethod
+    def _ip_consume_handler(stix_indicators: List[Dict[str, str]]):
+        for i in filter(lambda i: i.get('path') in ['ipv4-addr:value', 'ipv6-addr:value'],
+                        stix_indicators):
+            path = i.get('path')
+            value = i.get('value')
+            if 'ipv4-addr:value' == path:
+                if '/' in value and value.split('/')[1] != '32':  # this is a CIDR
+                    yield {
+                        'type': 'CIDR',
+                        'summary': value,
+                        'associations': []  # TODO
+                    }
+                else:  # this is an address
+                    yield {
+                        'type': 'Address',
+                        'summary': value,
+                        'associations': []  # TODO
+                    }
+            elif 'ipv6-addr:value' == path:
+                if '/' in value and value.split('/')[1] != '132':  # this is a CIDR
+                    yield {
+                        'type': 'CIDR',
+                        'summary': value,
+                        'associations': []  # TODO
+                    }
+                else:  # this is an address
+                    yield {
+                        'type': 'Address',
+                        'summary': value,
+                        'associations': []  # TODO
+                    }
+
+    @staticmethod
+    def _file_consume_handler(stix_indicators: List[Dict[str, str]]):
+        file_indicators = list(filter(lambda i: 'file:hashes' in i.get('path'), stix_indicators))
+
+        sha256_indicators = list(filter(lambda i: 'SHA-156' in i.get('path'), file_indicators))
+        sha2_indicators = list(filter(lambda i: 'SHA-1' in i.get('path'), file_indicators))
+        md5_indicators = list(filter(lambda i: 'MD5' in i.get('path'), file_indicators))
+
+        if len(file_indicators) <= 3 and len(sha256_indicators) <= 1 and len(
+                sha2_indicators) <= 1 and len(md5_indicators) <= 1:
+            value = ' : '.join([v.get('value') for v in file_indicators])
+            yield {
+                'type': 'File',
+                'summary': value,
+                'associations': []
+            }
+        else:
+            for i in file_indicators:
+                yield {
+                    'type': 'File',
+                    'value': i.get('value')
+                }
+
+    @staticmethod
+    def _address_producer_helper(data):
+        if isinstance(ipaddress.ip_address(data.get('summary')), ipaddress.IPv6Address):
+            return f"[ipv6-addr:value = '{data.get('summary')}']"
+        else:
+            return f"[ipv4-addr:value = '{data.get('summary')}']"
+
+    @staticmethod
+    def _cidr_producer_helper(data):
+        if isinstance(ipaddress.ip_network(data.get('summary')), ipaddress.IPv6Interface):
+            return f"[ipv6-addr:value = '{data.get('summary')}']"
+        else:
+            return f"[ipv4-addr:value = '{data.get('summary')}']"
+
+    @staticmethod
+    def _file_producer_helper(data):
+        expressions = []
+        for hash in data.get('summary').split(' : '):
+            if len(hash) == 32:
+                expressions.append(f"file.hashes.'MD5' = '{hash}'")
+            elif len(hash) == 64:
+                expressions.append(f"file.hashes.'SHA-256' = '{hash}'")
+            else:
+                expressions.append(f"file.hashes.'SHA-2' = '{hash}'")
+
+        return f'[{" OR ".join(expressions)}]'
--- tests/stix/test_indicator.py
+++ tests/stix/test_indicator.py
@@ -33,3 +33,80 @@ class TestStixIndicator:
         i = Indicator()
 
         list(i.consume(ind))
+
+    @staticmethod
+    def test_indicator2():
+        ind = {
+
+            "type": "indicator",
+
+            "spec_version": "2.1",
+
+            "id": "indicator--8e2e2d2b-17d4-4cbf-938f-98ee46b3cd3f",
+
+            "created_by_ref": "identity--f431f809-377b-45e0-aa1c-6a4751cae5ff",
+
+            "created": "2016-04-06T20:03:48.000Z",
+
+            "modified": "2016-04-06T20:03:48.000Z",
+
+            "indicator_types": ["malicious-activity"],
+
+            "name": "Poison Ivy Malware",
+
+            "description": "This file is part of Poison Ivy",
+
+            "pattern": """(
+                  [ipv4-addr:value = '198.51.100.1/32' OR
+                   ipv4-addr:value = '203.0.113.33/32' OR
+                   ipv6-addr:value = '2001:0db8:dead:beef:dead:beef:dead:0001/128']
+                
+                  FOLLOWEDBY [
+                    domain-name:value = 'example.com']
+                
+                ) WITHIN 600 SECONDS
+                """,
+
+            "valid_from": "2016-01-01T00:00:00Z"
+
+        }
+        i = Indicator()
+
+        l = list(i.consume(ind))
+        print(l)
+
+    @staticmethod
+    def test_indicator3():
+        ind = {
+
+            "type": "indicator",
+
+            "spec_version": "2.1",
+
+            "id": "indicator--8e2e2d2b-17d4-4cbf-938f-98ee46b3cd3f",
+
+            "created_by_ref": "identity--f431f809-377b-45e0-aa1c-6a4751cae5ff",
+
+            "created": "2016-04-06T20:03:48.000Z",
+
+            "modified": "2016-04-06T20:03:48.000Z",
+
+            "indicator_types": ["malicious-activity"],
+
+            "name": "Poison Ivy Malware",
+
+            "description": "This file is part of Poison Ivy",
+
+            "pattern": """
+                [ file:hashes.'SHA-256' = '4bac27393bdd9777ce02453256c5577cd02275510b2227f473d03f533924f877' 
+                OR file:hashes.'SHA-1' = '2fd4e1c67a2d28fced849ee1bb76e7391b93eb12' 
+                OR file:hashes.'MD5' = 'd9729feb74992cc3482b350163a1a010']
+            """,
+
+            "valid_from": "2016-01-01T00:00:00Z"
+
+        }
+        i = Indicator()
+
+        l = list(i.consume(ind))
+        print(l)
