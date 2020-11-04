d85297085e2e0cb2689b9f0cfa7c4b60ccc9690a
bpurdy <bpurdy@threatconnect.com>
2020-11-04 15:46:01 -0500

    removing some useless trace logs

--- tcex/stix/model.py
+++ tcex/stix/model.py
@@ -244,7 +244,6 @@ class StixModel:
 
         for data in tc_data:
             _type = indicator_type or data.get('type').lower()
-            self.logger.log.trace(f'indicator_type: {_type}')
             if _type not in self.indicator_type_details:
                 self.logger.log.info(f'unable to convert indicator type {_type} to stix object')
                 continue
