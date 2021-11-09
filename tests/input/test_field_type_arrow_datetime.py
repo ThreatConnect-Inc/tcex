eb18087deab9ade71e34e6f77598e6816c8cb656
Paco Huerta <phuerta@threatconnect.com>
2021-11-09 10:47:39 -0800

    Added initial handling of humanized date inputs

--- tests/input/test_field_type_arrow_datetime.py
+++ tests/input/test_field_type_arrow_datetime.py
@@ -1,9 +1,13 @@
 """Testing TcEx Input module field types."""
 # standard library
+from datetime import timedelta
+from operator import add, sub
 from typing import TYPE_CHECKING
 
 # third-party
+import arrow
 import pytest
+from dateutil.relativedelta import relativedelta
 from pydantic import BaseModel
 
 # first-party
@@ -39,9 +43,11 @@ class TestInputsFieldTypeArrowDateTime(InputTest):
             '2020/01',
             '2020.01',
             '2020',
+            '2020-01-01 00:00:00',
             # ISO 8601 still handled by default parser
             '2020-01-01T00:00:00+00:00',
-            '2020-01-01T00:00:00'
+            '2020-01-01T00:00:00Z',
+            '2020-01-01T00:00:00',
         ],
     )
     def test_field_type_arrow_date_time_simple(self, playbook_app: 'MockApp', to_parse):
@@ -80,7 +86,7 @@ class TestInputsFieldTypeArrowDateTime(InputTest):
         ],
     )
     def test_field_type_arrow_date_time_non_default_formats(
-        self, playbook_app: 'MockApp', to_parse
+            self, playbook_app: 'MockApp', to_parse
     ):
         """Testing inputs directly from built-in (non-default) formats section of docs.
 
@@ -179,6 +185,68 @@ class TestInputsFieldTypeArrowDateTime(InputTest):
         tcex.inputs.add_model(PytestModel)
 
         assert tcex.inputs.model.my_datetime.isoformat() == '2021-11-08T23:59:17.728000+00:00'
+
+    @pytest.mark.parametrize(
+        'to_parse,operator,delta',
+        [
+            # operator doesnt matter for 'now', as the time delta is 0
+            ('now', sub, timedelta(hours=0)),
+            ('1 years ago', sub, relativedelta(years=1)),
+            ('in 1 years', add, relativedelta(years=1)),
+            ('1 months ago', sub, relativedelta(months=1)),
+            ('in 1 months', add, relativedelta(months=1)),
+            ('2 weeks ago', sub, timedelta(weeks=2)),
+            ('in 2 weeks', add, timedelta(weeks=2)),
+            ('2 days ago', sub, timedelta(days=2)),
+            ('in 2 days', add, timedelta(days=2)),
+            ('1 hours ago', sub, timedelta(hours=1)),
+            ('in 1 hours', add, timedelta(hours=1)),
+            ('2 minutes ago', sub, timedelta(minutes=2)),
+            ('in 2 minutes', add, timedelta(minutes=2)),
+        ],
+    )
+    def test_field_type_arrow_date_time_human_input(
+            self, playbook_app: 'MockApp', to_parse, operator, delta
+    ):
+        """Testing timestamp inputs
+
+        Inputs are parsed relative to 'now' in UTC. The parsed input will be compared to a
+        datetime object that is calculated outside of TCEX. Note that a small amount of play
+        is allowed when comparing minutes in case minutes change between the time the test starts
+        and the time the date is parsed.
+
+        :param to_parse: the human date time to parse.
+        :param operator: either an add or subtract operator that will be used to calculate
+        the expected value
+        :param delta: the time delta object to use to calculate the approximate value that should
+        result from the parsing of the human date time string.
+        """
+
+        now = arrow.utcnow()
+
+        class PytestModel(BaseModel):
+            """Test Model for Inputs"""
+
+            my_datetime: ArrowDateTime
+
+        config_data = {'my_datetime': to_parse}
+        app = playbook_app(config_data=config_data)
+        tcex = app.tcex
+        tcex.inputs.add_model(PytestModel)
+
+        expected_parse_result = (operator(now, delta))
+        result = tcex.inputs.model.my_datetime
+
+        assert expected_parse_result.year == result.year
+        assert expected_parse_result.month == result.month
+        assert expected_parse_result.day == result.day
+        assert expected_parse_result.hour == result.hour
+        assert expected_parse_result.tzinfo == result.tzinfo
+
+        # allow a bit of play in minutes in case the time changes between the time the 'now'
+        # variable of this test case is calculated and the time that TCEX parses the human input.
+        minute_difference = abs(expected_parse_result.minute - result.minute)
+        assert minute_difference == 0 or minute_difference == 1
 
     def test_field_type_arrow_date_time_seconds(self, playbook_app: 'MockApp'):
         """Testing timestamp inputs. Parse seconds epoch"""
