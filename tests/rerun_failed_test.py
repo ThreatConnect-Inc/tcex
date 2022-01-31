0cc02b1196c095e8137d03edb3677350f781cbf9
Bracey Summers <bsummers@threatconnect.com>
2022-01-31 11:46:54 -0600

    + updated rerun_failed_tests.py to only run as main.

--- tests/rerun_failed_test.py
+++ tests/rerun_failed_test.py
@@ -7,17 +7,25 @@ import sys
 # third-party
 import pytest
 
-try:
-    with open(os.path.join(os.getcwd(), '.pytest_cache', 'v', 'cache', 'lastfailed')) as f:
-        data = json.load(f)
-        tests = list(data.keys())
-except Exception as ex:
-    raise RuntimeError('Could not fetch list of failed tests from last run.') from ex
 
-newline = '\n'
-print(
-    f'Executing last failed tests serially. Tests to execute ({len(tests)}):\n{newline.join(tests)}'
-)
-print(tests)
+def rerun():
+    """Rerun failed tests."""
 
-sys.exit(pytest.main(tests))
+    try:
+        failed_file = os.path.join(os.getcwd(), '.pytest_cache', 'v', 'cache', 'lastfailed')
+        with open(failed_file) as f:
+            data = json.load(f)
+            tests = list(data.keys())
+    except Exception as ex:
+        raise RuntimeError('Could not fetch list of failed tests from last run.') from ex
+
+    newline = '\n'
+    print(
+        'Executing last failed tests serially. Tests '
+        f'to execute ({len(tests)}):\n{newline.join(tests)}'
+    )
+    sys.exit(pytest.main(tests))
+
+
+if __name__ == '__main__':
+    rerun()
